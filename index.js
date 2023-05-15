33a20492282e4f986374f876894fba3d5f6d0794
granth2023 <grantharris38@gmail.com>
2023-05-15 18:36:57 -0400

    publish

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node 
 const fs = require('fs')
 
-fs.readFile(__NPM_Resume + '/info.json', 'utf8', function(err, data) {
+fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
     if (err) {
         console.log(err)
     } else {

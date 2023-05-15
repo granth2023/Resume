c52443519136f62406354322bd6b24856218a963
granth2023 <grantharris38@gmail.com>
2023-05-15 15:41:19 -0400

    resume

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node 
+const fs = require('fs')
+
+fs.readFile(npm_resume + '/info.json', 'utf8', function(err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+        return data
+    }
+})
